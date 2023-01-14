f84bde9601e22429d0bd1b885ca1e804925a9854
git-tolu <adejumobitoluwanimi2@gmail.com>
2023-01-14 22:14:23 +0100

    info list commmit

---
+++ ticketbooking/lib/utils/app_info_list.dart
@@ -0,0 +1,20 @@
+List<Map<String, dynamic>> hotellist = [
+  {
+    'image': 'one.png',
+    'place': 'open space',
+    'destination': 'london',
+    'price': 25
+  },
+  {
+    'image': 'two.png',
+    'place': 'global will',
+    'destination': 'london',
+    'price': 40
+  },
+  {
+    'image': 'three.png',
+    'place': 'tallest building',
+    'destination': 'dubai',
+    'price': 68
+  }
+];
